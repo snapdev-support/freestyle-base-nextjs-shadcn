104a2a631f5752d641e13c4f00ade1e45f20e1f6
Tayyab Ali <tayyabali124534@gmail.com>
2025-12-08 15:48:20 +0500

    Added error capture script

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script src="/error-capture.js" strategy="beforeInteractive" />
         {children}
       </body>
     </html>
